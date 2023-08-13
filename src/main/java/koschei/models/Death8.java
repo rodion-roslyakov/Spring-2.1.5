3b8335c0d030b122006efd113e559e2452b81969
Rodion <roslyakov_rodion@mail.ru>
2023-08-13 18:33:53 +0300

    Тесты пройдены!

--- src/main/java/koschei/models/Death8.java
+++ src/main/java/koschei/models/Death8.java
@@ -1,12 +1,8 @@
 package koschei.models;
 
-import koschei.KoscheiTheDeathless;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Death8 {
-    @Autowired
-    public Death8(KoscheiTheDeathless koscheiTheDeathless) {
-    }
+
 }
